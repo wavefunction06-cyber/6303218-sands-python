0c29ed130fb02478137df724a28c81982efe6b86
crisp <wavefunction06@gmail.com>
2025-09-29 16:57:25 +0200

    New wave

--- sine.py
+++ sine.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-freq=3
-duration=10
-samplerate=1000
-
-t = np.arange(0, duration, 1/samplerate)
-wave = np.sin(2*np.pi*freq*t)
-
-plt.plot(t,wave)
-plt.title(f"sine wave: {freq} Hz, {duration} s")
-plt.xlabel("Time [s]")
-plt.ylabel("Amplitude")
-plt.show()
+def generate_sine_wave(frequency=1, duration=5, sample_rate=1000):
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    y = np.sin(2 * np.pi * frequency * t)
+    
+    plt.plot(t, y)
+    plt.title(f'Sine Wave: {frequency} Hz')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.grid()
+    plt.show()
+generate_sine_wave()
